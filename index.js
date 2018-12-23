17cdcb8ce542388195b9c3844266749fa2cc88bb
Alex Zdatny <alexzdatny@gmail.com>
2018-12-23 20:47:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function getFirstSelector(selector) {
 function nestedTarget(cl, id) {
   return document.querySelector('#nested .target');
 }
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let secnode = node.children[0]
+
+  while (secnode) {
+    node = secnode
+    secnode = node.children[0]
+  }
+
+  return node
+}
